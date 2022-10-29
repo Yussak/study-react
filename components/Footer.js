178a38a11d026144653a14900011dbec73d108b2
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-10-29 23:27:44 +0900

    💄スタイルファイルの分割

--- components/Footer.js
+++ components/Footer.js
@@ -1,17 +1,17 @@
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import classes from "./Footer.module.css";
 
 export function Footer() {
   return (
-    <div className={styles.container}>
-      <footer className={styles.footer}>
+    <div className={classes.container}>
+      <footer className={classes.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by{" "}
-          <span className={styles.logo}>
+          <span className={classes.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
