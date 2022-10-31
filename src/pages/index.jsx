2b695e7876926aabe291d94403b3ce84a623ed49
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-10-31 21:01:14 +0900

    ✨ 練習用のuseEffect導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,16 +3,26 @@ import styles from "src/styles/Home.module.css";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
 import { Footer } from "src/components/Footer";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 
 export default function Home() {
   const foo = 1;
 
-  // component内でuseCallbackを使うことで再レンダリング時にメソッドの再生成を防止
+  // component内でuseCallbackを使うことで再レンダリング時にメソッドの再生成を防止;
   const handleClick = useCallback((e) => {
     console.log(e.target.href);
     e.preventDefault();
     alert(foo);
+  }, []);
+
+  useEffect(() => {
+    // マウント時
+    document.body.style.backgroundColor = "lightblue";
+
+    // アンマウント時
+    return () => {
+      document.body.style.backgroundColor = "";
+    };
   }, []);
 
   return (
