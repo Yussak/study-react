f4e1ee67066d5d1967987c2046eb1fe54ab32c7b
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-10-31 20:16:04 +0900

    ⚗️ 試しにクリックイベントを記述

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,8 +3,18 @@ import styles from "src/styles/Home.module.css";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
 import { Footer } from "src/components/Footer";
+import { useCallback } from "react";
 
 export default function Home() {
+  const foo = 1;
+
+  // component内でuseCallbackを使うことで再レンダリング時にメソッドの再生成を防止
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,7 +22,9 @@ export default function Home() {
       </Head>
 
       <Header />
-
+      <a href="/about" onClick={handleClick}>
+        button
+      </a>
       <Main page="index" />
       <Footer />
     </div>
