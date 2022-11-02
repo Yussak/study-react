7e61239aa7764fceeabc10882f50a30fccddc5cb
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-11-02 20:28:31 +0900

    ⚡️useCallback,useEffectの練習

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,16 +3,17 @@ import styles from "src/styles/Home.module.css";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
 import { Footer } from "src/components/Footer";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
-  const [foo, setFoo] = useState(1);
+  const [count, setCount] = useState(1);
 
   // component内でuseCallbackを使うことで再レンダリング時にメソッドの再生成を防止;
-  const handleClick = (e) => {
-    setFoo((foo) => foo + 1);
-    setFoo((foo) => foo + 1);
-  };
+  const handleClick = useCallback(() => {
+    if (count < 10) {
+      setCount((count) => count + 1);
+    }
+  }, [count]);
 
   useEffect(() => {
     // マウント時
@@ -30,7 +31,7 @@ export default function Home() {
         <title>Index Page</title>
       </Head>
 
-      <h1>{foo}</h1>
+      <h1>{count}</h1>
       <Header />
       <button href="/about" onClick={handleClick}>
         button
